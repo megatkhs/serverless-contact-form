a590084f103845b226f47184567bf3e0a0255884
megatkhs <tarodino0526@gmail.com>
2022-08-16 00:56:53 +0900

    [add] リセットCSSを追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
+import path from 'path'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: /~(.+)/,
+        replacement: path.join(__dirname, 'node_modules/$1'),
+      },
+      {
+        find: /@\//,
+        replacement: path.join(__dirname, 'src/'),
+      },
+    ],
+  },
 })
